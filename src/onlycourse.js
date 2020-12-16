666386caf8ad080955f5eae504704d2e5e9b0be4
Kallinikos Cholevas <k.cholevas@outlook.com>
2020-12-16 18:59:35 +0200

    Update onlycourse.js

--- src/onlycourse.js
+++ src/onlycourse.js
@@ -12,6 +12,7 @@ import Button from "react-bootstrap/Button";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
+import Modal from 'react-bootstrap/Modal'
 
 const Course = ({match}) => {
   
@@ -50,6 +51,11 @@ const [item,setItem] = useState([])
   const DateFormatter = (date) => {
     return new Date(date).toLocaleDateString("en-US");
   };
+
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
 
   return (
     <div>
@@ -95,8 +101,24 @@ const [item,setItem] = useState([])
       </div>
 
       <div className="buttons">
-        <Button variant="primary">Edit</Button>{" "}
+        <Button variant="primary" onClick={handleShow}>Edit</Button>{" "}
         <Button variant="danger">Delete</Button>{" "}
+
+        <Modal show={show} onHide={handleClose} animation={false}>
+        <Modal.Header closeButton>
+          <Modal.Title>Modal heading</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={handleClose}>
+            Save Changes
+          </Button>
+        </Modal.Footer>
+      </Modal>
+      
       </div>
 
       <div>
