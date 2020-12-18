bc4766a3569ea4b19f6c3e988354b20112bc8cfe
Kallinikos Cholevas <k.cholevas@outlook.com>
2020-12-18 19:03:20 +0200

    Update Instructor.js

--- src/Instructor.js
+++ src/Instructor.js
@@ -1,11 +1,7 @@
-import React, { useState, useEffect, Component } from "react";
-import ReactDOM from "react-dom";
-import db from "./db";
+import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.css";
-import Container from "react-bootstrap/Container";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
+
 
 const Instructor = (props) => {
   return (
