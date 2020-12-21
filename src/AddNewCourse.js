b5c1a11a0d96a99ecbad1c5699cf6e2e0e9b25f4
panagiotachristou <74738409+panagiotachristou@users.noreply.github.com>
2020-12-22 00:53:30 +0200

    Update AddNewCourse.js

--- src/AddNewCourse.js
+++ src/AddNewCourse.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { Form, Jumbotron, Alert, Spinner, Button } from "react-bootstrap";
 import axios from "axios";
-
 import { API } from "./api";
 
 const AddNewCourse = () => {
@@ -29,40 +28,36 @@ const AddNewCourse = () => {
   const { normal, early_bird } = price;
 
   useEffect(() => {
-    const fetchData = () => {
+    const fetchData = async () => {
       setError(false);
       setIsLoading(true);
-
-      axios
-        .get(`${API}/courses`)
-        .then((response) => {
-          setCourses(response.data);
-          setIsLoading(false);
-        })
-        .catch((error) => {
-          setError(error);
-          setIsLoading(false);
-        });
+      try {
+        const response = await axios.get(`${API}/courses`);
+        setCourses(response.data);
+        setIsLoading(false);
+      } catch {
+        setError(error);
+        setIsLoading(false);
+      }
     };
 
     fetchData();
   }, []);
 
   useEffect(() => {
-    const fetchData = () => {
+    const fetchData = async () => {
       setError(false);
       setIsLoading(true);
-      axios
-        .get(`${API}/instructors`)
-        .then((response) => {
-          setDBInstructors(response.data);
-          setIsLoading(false);
-        })
-        .catch((error) => {
-          setError(error);
-          setIsLoading(false);
-        });
+      try {
+        const response = await axios.get(`${API}/instructors`);
+        setDBInstructors(response.data);
+        setIsLoading(false);
+      } catch {
+        setError(error);
+        setIsLoading(false);
+      }
     };
+
     fetchData();
   }, []);
 
@@ -110,7 +105,7 @@ const AddNewCourse = () => {
       });
     } else if (id === `bookable`) {
       const checked = event.target.checked;
-
+      
       if (checked) {
         setOpen(true);
       } else {
@@ -121,176 +116,177 @@ const AddNewCourse = () => {
     }
   };
 
-  const AddCourse = (data) => {
-
+  const AddCourse = async (data) => {
     const newData = {
       ...data,
       id: Math.max(...courses.map(({ id }) => id)) + 1,
     };
 
-    axios.post(`${API}/courses`, newData).then((res) => {
+    try {
+      await axios.post(`${API}/courses`, newData);
       window.location.href = `/courses`;
-    });
+    } catch (err) {
+      setError(error);
+    }
   };
 
   return (
     <div>
-      
-        <Jumbotron>
-          <Form>
-            <h2>Add Course</h2>
-            {[
-              {
-                field: "title",
-                state: title,
-                type: "text",
-                placeholder: "Title",
-                label: "Title",
-                setState: setTitle,
-              },
-              {
-                field: "duration",
-                state: duration,
-                type: "text",
-                placeholder: "Duration",
-                label: "Duration",
-                setState: setDuration,
-              },
-              {
-                field: "imagePath",
-                state: imagePath,
-                type: "text",
-                placeholder: "Image path",
-                label: "Image path",
-                setState: setImagePath,
-              },
-            ].map(({ field, state, type, placeholder, label, setState }) => (
-              <Form.Group key={field} controlId={field}>
-                <Form.Label>{label}</Form.Label>
-                <Form.Control
-                  placeholder={placeholder}
-                  type={type}
-                  value={state}
-                  name={field}
-                  onChange={(e) => onInputChange(e, setState)}
-                />
-              </Form.Group>
-            ))}
-            <Form.Check
-              type={`checkbox`}
-              id={`bookable`}
-              label={`Bookable`}
-              value={open}
-              onChange={(e) => onInputChange(e, setOpen)}
-            />
-            <hr className="my-2" />
-            <h2>Instructors</h2>
-            {dbInstructors.map(({ id, name }) => (
-              <Form.Check
-                type={`checkbox`}
-                value={id}
-                name={id}
-                key = {id}
-                label={`${name.first}  ${name.last}`}
-                checked={instructors.includes({ id })}
-                onChange={handleChange}
-              />
-            ))}
-
-            <hr className="my-2" />
-            <Form.Group controlId="formCourseDescription">
-              <Form.Label>Description</Form.Label>
+      <Jumbotron>
+        <Form>
+          <h2>Add Course</h2>
+          {[
+            {
+              field: "title",
+              state: title,
+              type: "text",
+              placeholder: "Title",
+              label: "Title",
+              setState: setTitle,
+            },
+            {
+              field: "duration",
+              state: duration,
+              type: "text",
+              placeholder: "Duration",
+              label: "Duration",
+              setState: setDuration,
+            },
+            {
+              field: "imagePath",
+              state: imagePath,
+              type: "text",
+              placeholder: "Image path",
+              label: "Image path",
+              setState: setImagePath,
+            },
+          ].map(({ field, state, type, placeholder, label, setState }) => (
+            <Form.Group key={field} controlId={field}>
+              <Form.Label>{label}</Form.Label>
               <Form.Control
-                as="textarea"
-                rows={3}
-                value={description}
-                onChange={(e) => onInputChange(e, setDescription)}
+                placeholder={placeholder}
+                type={type}
+                value={state}
+                name={field}
+                onChange={(e) => onInputChange(e, setState)}
               />
             </Form.Group>
-            <hr className="my-2" />
-            <h2>Dates</h2>
-            {[
-              {
-                field: "start_date",
-                state: start_date,
-                type: "date",
-                placeholder: "Start date",
-                label: "Start date",
-                setState: setDate,
-              },
-              {
-                field: "end_date",
-                state: end_date,
-                type: "date",
-                placeholder: "End date",
-                label: "End date",
-                setState: setDate,
-              },
-            ].map(({ field, state, type, placeholder, label, setState }) => (
-              <Form.Group key={field} controlId={field}>
-                <Form.Label>{label}</Form.Label>
-                <Form.Control
-                  placeholder={placeholder}
-                  type={type}
-                  value={state}
-                  name={field}
-                  onChange={(e) => onInputChange(e, setState)}
-                />
-              </Form.Group>
-            ))}
-            <hr className="my-2" />
-            <h2>Price</h2>
-            {[
-              {
-                field: "early_bird",
-                state: early_bird,
-                type: "number",
-                placeholder: "Early bird",
-                label: "Early bird",
-                setState: setPrice,
-              },
-              {
-                field: "normal",
-                state: normal,
-                type: "number",
-                placeholder: "Normal Price",
-                label: "Normal Price",
-                setState: setPrice,
-              },
-            ].map(({ field, state, type, placeholder, label, setState }) => (
-              <Form.Group key={field} controlId={field}>
-                <Form.Label>{label}</Form.Label>
-                <Form.Control
-                  placeholder={placeholder}
-                  type={type}
-                  value={state}
-                  name={field}
-                  onChange={(e) => onInputChange(e, setState)}
-                />
-              </Form.Group>
-            ))}
-            <hr className="my-2" />
-            <Button
-              style={{ float: "right" }}
-              color="primary"
-              onClick={() =>
-                AddCourse({
-                  title,
-                  duration,
-                  imagePath,
-                  open,
-                  price,
-                  dates,
-                  instructors,
-                  description,
-                })
-              }
-            >
-              Add Course
-            </Button>
-          </Form>
-        </Jumbotron>
-      
+          ))}
+          <Form.Check
+            type={`checkbox`}
+            id={`bookable`}
+            label={`Bookable`}
+            value={open}
+            checked={open}
+            onChange={(e) => onInputChange(e, setOpen)}
+          />
+          <hr className="my-2" />
+          <h2>Instructors</h2>
+          {dbInstructors.map(({ id, name }) => (
+            <Form.Check
+              type={`checkbox`}
+              value={id}
+              name={id}
+              key={id}
+              label={`${name.first}  ${name.last}`}
+              checked={instructors.includes(id)}
+              onChange={handleChange}
+            />
+          ))}
+
+          <hr className="my-2" />
+          <Form.Group controlId="formCourseDescription">
+            <Form.Label>Description</Form.Label>
+            <Form.Control
+              as="textarea"
+              rows={3}
+              value={description}
+              onChange={(e) => onInputChange(e, setDescription)}
+            />
+          </Form.Group>
+          <hr className="my-2" />
+          <h2>Dates</h2>
+          {[
+            {
+              field: "start_date",
+              state: start_date,
+              type: "date",
+              placeholder: "Start date",
+              label: "Start date",
+              setState: setDate,
+            },
+            {
+              field: "end_date",
+              state: end_date,
+              type: "date",
+              placeholder: "End date",
+              label: "End date",
+              setState: setDate,
+            },
+          ].map(({ field, state, type, placeholder, label, setState }) => (
+            <Form.Group key={field} controlId={field}>
+              <Form.Label>{label}</Form.Label>
+              <Form.Control
+                placeholder={placeholder}
+                type={type}
+                value={state}
+                name={field}
+                onChange={(e) => onInputChange(e, setState)}
+              />
+            </Form.Group>
+          ))}
+          <hr className="my-2" />
+          <h2>Price</h2>
+          {[
+            {
+              field: "early_bird",
+              state: early_bird,
+              type: "number",
+              placeholder: "Early bird",
+              label: "Early bird",
+              setState: setPrice,
+            },
+            {
+              field: "normal",
+              state: normal,
+              type: "number",
+              placeholder: "Normal Price",
+              label: "Normal Price",
+              setState: setPrice,
+            },
+          ].map(({ field, state, type, placeholder, label, setState }) => (
+            <Form.Group key={field} controlId={field}>
+              <Form.Label>{label}</Form.Label>
+              <Form.Control
+                placeholder={placeholder}
+                type={type}
+                value={state}
+                name={field}
+                onChange={(e) => onInputChange(e, setState)}
+              />
+            </Form.Group>
+          ))}
+        </Form>
+        <hr className="my-2" />
+        <Button
+          style={{ float: "right" }}
+          color="primary"
+          onClick={() =>
+            AddCourse({
+              title,
+              duration,
+              imagePath,
+              open,
+              price,
+              dates,
+              instructors,
+              description,
+            })
+          }
+        >
+          Add Course
+        </Button>
+      </Jumbotron>
     </div>
   );
 };
