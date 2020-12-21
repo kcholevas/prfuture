9f1de0ab7b414da7049dbf1006935d4790833b05
panagiotachristou <74738409+panagiotachristou@users.noreply.github.com>
2020-12-21 19:10:29 +0200

    Added by Panagiota

--- src/dashboard.js
+++ src/dashboard.js
@@ -1,23 +1,42 @@
 import React, { useState, useEffect } from "react";
 import CoursesTable from "./CoursesTable";
 import "bootstrap/dist/css/bootstrap.min.css";
-import Card from "react-bootstrap/Card";
-import { Jumbotron } from "reactstrap";
+import { Card, Jumbotron, Alert, Spinner } from "react-bootstrap";
 import "./index.css";
+import axios from "axios";
+import { API } from "./api";
 
 function Dashboard() {
+  const [stats, setStats] = useState([]);
+  const [data, SetData] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
   useEffect(() => {
-    fetchStats();
+    const fetchData = async () => {
+      setError(false);
+      setIsLoading(true);
+      try {
+        const response = await axios.get(`${API}/stats`);
+        setStats(response.data);
+        setIsLoading(false);
+        SetData(true);
+      } catch {
+        setError(error);
+        setIsLoading(false);
+      }
+    };
+
+    fetchData();
   }, []);
 
-  const [stats, setStats] = useState([]);
+  if (error) {
+    return <Alert variant="warning">{error.message}</Alert>;
+  }
 
-  const fetchStats = async () => {
-    const res = await fetch(`http://localhost:3001/stats`);
-    const stats = await res.json(); //oxi mono ta stats genika ola ta data
-    console.log(stats);
-    setStats(stats);
-  };
+  if (isLoading) {
+    return <Spinner animation="border" size="lg" />;
+  }
 
   // Intro
   // ========================================
@@ -34,21 +53,20 @@ function Dashboard() {
 
   // Stats
   // ========================================
-  debugger;
   const StatsList = () => {
-    return stats.map((stats) => {
-      return (
-        <Card
-          key={stats.id}
-          style={{ width: "12rem", display: "inline-block" }}
-        >
-          <Card.Body>
-            <Card.Title>{stats.title.toUpperCase()}</Card.Title>
-            <Card.Text>{stats.amount}</Card.Text>
-          </Card.Body>
-        </Card>
-      );
-    });
+    return data
+      ? stats.map((stats) => {
+          <Card
+            key={stats.id}
+            style={{ width: "12rem", display: "inline-block" }}
+          >
+            <Card.Body>
+              <Card.Title>{stats.title.toUpperCase()}</Card.Title>
+              <Card.Text>{stats.amount}</Card.Text>
+            </Card.Body>
+          </Card>;
+        })
+      : "Loading";
   };
 
   return (
