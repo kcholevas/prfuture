0b67929bcb1ab8e3c42857e91a7875361433ebd8
panagiotachristou <74738409+panagiotachristou@users.noreply.github.com>
2020-12-22 14:13:03 +0200

    Update CoursesTable.js

--- src/CoursesTable.js
+++ src/CoursesTable.js
@@ -4,15 +4,16 @@ import { Table, Button, Alert, Spinner } from "react-bootstrap";
 import axios from "axios";
 import "./index.css";
 import { API } from "./api";
-
+import { FcCheckmark, FcCancel } from "react-icons/fc";
 
 // Table of courses
 // ========================================
- 
+
 function CoursesTable() {
   const [courses, setItems] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
+  const [data, SetData] = useState(false);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -22,6 +23,7 @@ function CoursesTable() {
         const response = await axios.get(`${API}/courses`);
         setItems(response.data);
         setIsLoading(false);
+        SetData(true);
       } catch {
         setError(error);
         setIsLoading(false);
@@ -31,6 +33,14 @@ function CoursesTable() {
     fetchData();
   }, []);
 
+  const checkBookable = (props) => {
+    if (props.children) {
+      return <FcCheckmark />;
+    } else {
+      return <FcCancel />;
+    }
+  };
+
   if (error) {
     return <Alert variant="warning">{error.message}</Alert>;
   }
@@ -39,43 +49,133 @@ function CoursesTable() {
     return <Spinner animation="border" size="lg" />;
   }
 
-
   const DateFormatter = (date) => {
     return new Date(date).toLocaleDateString("en-US");
   };
-    return (
-        <div>Last 5 courses
-            <Table>
-            <thead>
-            <tr>
-                <th style={{ width: "20%", justifyContent: "center", textAlign: "left" }}>Title</th>
-                <th style={{ width: "20%", justifyContent: "center", textAlign: "left" }}>Bookable</th>
-                <th style={{ width: "20%", justifyContent: "center", textAlign: "left" }}>Price</th>
-                <th style={{ width: "20%", justifyContent: "center", textAlign: "left" }}>Date</th>
-                <th style={{ width: "20%", justifyContent: "center", textAlign: "left" }}>Actions</th>
-            </tr>
-            </thead>
-            
-            </Table>
-            {courses.slice(courses, 5).map((courses)=> {
-            return <div key={courses.id}>
+
+  return data ? (
+    <div>
+      Last 5 courses
+      <Table>
+        <thead>
+          <tr>
+            <th
+              style={{
+                width: "20%",
+                justifyContent: "center",
+                textAlign: "left",
+              }}
+            >
+              Title
+            </th>
+            <th
+              style={{
+                width: "20%",
+                justifyContent: "center",
+                textAlign: "left",
+              }}
+            >
+              Bookable
+            </th>
+            <th
+              style={{
+                width: "20%",
+                justifyContent: "center",
+                textAlign: "left",
+              }}
+            >
+              Price
+            </th>
+            <th
+              style={{
+                width: "20%",
+                justifyContent: "center",
+                textAlign: "left",
+              }}
+            >
+              Date
+            </th>
+            <th
+              style={{
+                width: "20%",
+                justifyContent: "center",
+                textAlign: "left",
+              }}
+            >
+              Actions
+            </th>
+          </tr>
+        </thead>
+      </Table>
+      {courses.map(({ id, title, open, price, dates }) => {
+        return (
+          <div key={id}>
             <Table responsive hover>
-            <tbody>
-            <tr>
-                <th style={{ width: "20%", justifyContent: "center", textAlign: "left" }}>{courses.title}</th>
-                <th style={{ width: "20%", justifyContent: "center", textAlign: "left" }}><img className="photo" src={process.env.PUBLIC_URL + '/check.png'} alt="Bookable"/></th>
-                <th style={{ width: "20%", justifyContent: "center", textAlign: "left" }}>{courses.price.normal}</th>
-                <th style={{ width: "20%", justifyContent: "center", textAlign: "left" }}>{`${DateFormatter(courses.dates.start_date)} - ${DateFormatter(courses.dates.end_date)}`}</th>
-                <th><Button color="info" onClick={event =>  window.location.href=(`/courses/${courses.id}`)}>View Details</Button>{' '}</th>
-            </tr>
-            </tbody>
-        </Table>
-            </div>
-            })}<Button className="float-right" color="primary" onClick={event =>  window.location.href='/courses'}>View All</Button>{' '}
-        </div>
-
-    );
+              <tbody>
+                <tr>
+                  <th
+                    style={{
+                      width: "20%",
+                      justifyContent: "center",
+                      textAlign: "left",
+                    }}
+                  >
+                    {title}
+                  </th>
+                  <th
+                    style={{
+                      width: "20%",
+                      justifyContent: "center",
+                      textAlign: "left",
+                    }}
+                  >
+                    {checkBookable(open)}
+                  </th>
+                  <th
+                    style={{
+                      width: "20%",
+                      justifyContent: "center",
+                      textAlign: "left",
+                    }}
+                  >
+                    {price.normal}
+                  </th>
+                  <th
+                    style={{
+                      width: "20%",
+                      justifyContent: "center",
+                      textAlign: "left",
+                    }}
+                  >{`${DateFormatter(dates.start_date)} - ${DateFormatter(
+                    dates.end_date
+                  )}`}</th>
+                  <th>
+                    <Button
+                      color="info"
+                      onClick={(event) =>
+                        (window.location.href = `/courses/${id}`)
+                      }
+                    >
+                      View Details
+                    </Button>
+                  </th>
+                </tr>
+              </tbody>
+            </Table>
+          </div>
+        );
+      })}
+      <Button
+        className="float-right"
+        color="primary"
+        onClick={(event) => (window.location.href = "/courses")}
+      >
+        View All
+      </Button>
+    </div>
+  ) : (
+    <Spinner animation="border" size="lg" />
+  );
 }
-
 
 export default CoursesTable;
